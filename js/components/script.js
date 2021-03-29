f8f87a7b913a23ed32850474c7cf7ad4f1501918
PdePH <paulohsouza@uni9.edu.br>
2021-03-29 17:23:15 -0300

    Update style css

--- js/components/script.js
+++ js/components/script.js
@@ -1,3 +1,4 @@
+// main-form
 var form = document.getElementById("contact");
 
 async function handleSubmit(event) {
@@ -20,3 +21,20 @@ async function handleSubmit(event) {
     });
 }
 form.addEventListener("submit", handleSubmit)
+
+// main-carousel
+
+const buttonRight = document.querySelector('.button-arrow.-right');
+const buttonLeft = document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+let pixels = 0;
+
+buttonRight.addEventListener('click', function() {
+    if (pixels == -600) { return; } else { pixels = pixels - 100; }
+    elements.style = `transform: translateX(${pixels}px);`;
+});
+
+buttonLeft.addEventListener('click', function() {
+    if (pixels == 0) { return; } else { pixels = pixels + 100; }
+    elements.style = `transform: translateX(${pixels}px);`;
+});
